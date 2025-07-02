34e35c209f65fa0cd690e5768fb305adb1299227
VasavMittal <vasavmittal30@gmail.com>
2025-07-02 15:41:24 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -10,10 +10,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => {
   console.log("✅ MongoDB connected");
 
